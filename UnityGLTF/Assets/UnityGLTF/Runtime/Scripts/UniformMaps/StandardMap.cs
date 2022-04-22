87513bcc0db90b46b70685589919902f854117d0
David Gilbert <gilbert@vr.rwth-aachen.de>
2022-04-22 14:13:31 +0200

    Added missing using UnityEditor.

--- UnityGLTF/Assets/UnityGLTF/Runtime/Scripts/UniformMaps/StandardMap.cs
+++ UnityGLTF/Assets/UnityGLTF/Runtime/Scripts/UniformMaps/StandardMap.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using Material = UnityEngine.Material;
 using Texture = UnityEngine.Texture;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace UnityGLTF
 {
